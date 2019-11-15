b1aa7f3dcae30148e7c82327b35dd02802ff6279
Aoxue10 <56706576+Aoxue10@users.noreply.github.com>
2019-11-15 12:20:58 +0800

    人物動作2

--- UnityProject/Assets/script/player.cs
+++ UnityProject/Assets/script/player.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 public class player : MonoBehaviour
-{
+{ 
     [Header("速度"),Range(0f, 100f)]
     public float speed = 3.5f;
     [Header("跳躍"), Range(100, 2000)]
@@ -11,4 +11,39 @@ public class player : MonoBehaviour
     public bool isGround = false;
     [Header("角色名稱")]
     public string name = "KID";
+    [Header("2D鋼體")]
+    public Rigidbody2D r2d;
+    public Animator ani;
+    //#endregion
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        r2d.AddForce(new Vector2(speed*h, 0));
+        ani.SetBool("跑步開關", h != 0);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&isGround==true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
+    }
+    private void Dead()
+    {
+
+    }
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Tilemap")
+        {
+            isGround = true;
+        }
+    }
 }
